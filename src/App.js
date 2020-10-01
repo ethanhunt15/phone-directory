dc584a93fba7733fc4ddaf5f2b4bcb062feaf597
ethanhunt15 <amitshrivastava15@yahoo.com>
2020-10-01 18:58:50 +0530

    modified app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header'
 
-function App() {
-  return (
-    <div>
+// function App() {
+//   return (
+//     <div>
       
-    </div>
-  );
+//     </div>
+//   );
+// }
+
+
+class App extends Component
+{
+  render()
+  {
+    return(
+      <div>
+        <Header />
+        <button> Add </button>
+        <div>
+          <span> Name </span><br />
+          <span> Phone </span>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
