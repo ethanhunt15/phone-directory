d48b7d6b0fdd0ae5efb443df45109cb43c306769
ethanhunt15 <amitshrivastava15@yahoo.com>
2020-10-01 18:56:34 +0530

    Created Header Component

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+const Header = function(){
+    return(
+        <div class="header">
+            Phone Directory
+            </div>
+    )
+}
+
+// class Header extends Component
+// {
+//         render()
+//         {
+//             return(<div class="header">
+//             Phone Directory
+//             </div>)
+//         }
+    
+// }
+
+
+
+export default Header
